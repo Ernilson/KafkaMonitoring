a20ac9d2d2f804e8c04a955a27787c1c8f0129ed
Ernilson Daniel <ernilsondaniel@sisconsultoria.com.br>
2022-03-28 09:22:05 -0300

    monitoramento de grupos

---
+++ str-produce/src/main/java/br/com/strproduce/StrProduceApplication.java
@@ -0,0 +1,13 @@
+package br.com.strproduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProduceApplication.class, args);
+	}
+
+}
---
+++ str-produce/src/test/java/br/com/strproduce/StrProduceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.strproduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
